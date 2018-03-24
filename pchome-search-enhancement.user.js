45154d900dc33604ba0226506f0e05707ae6e913
Gea-Suan Lin <gslin@gslin.org>
2018-03-25 00:03:04 +0800

    Change to let.

--- pchome-search-enhancement.user.js
+++ pchome-search-enhancement.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PChome search enhancement
 // @namespace    https://wiki.gslin.org/wiki/PChomeSearchEnhancement
-// @version      20180324.1
+// @version      20180324.2
 // @description  Ignore non-buyable items in PChome search page.
 // @author       Gea-Suan Lin <darkkiller@gmail.com>
 // @license      MIT
@@ -12,10 +12,10 @@
 (function() {
     'use strict';
 
-    var content = document.getElementById('CONTENT');
-    var tr = 1 / 3;
+    let content = document.getElementById('CONTENT');
+    let tr = 1 / 3;
 
-    var ob = new window.MutationObserver(function(el){
+    let ob = new window.MutationObserver(function(el){
         window.jQuery('.col3f:has(li.orderNotyet), .col3f:has(li.orderReplenish), .col3f:has(li.soldOut)', el.target).css('opacity', tr);
     });
 
